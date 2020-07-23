086f52e8a9d7c8bb07174abb721d6e1a80db693f
rhonyshniuk <roman.honyshniuk@datarobot.com>
2020-07-23 09:54:57 +0300

    Create selenium_form.py

---
+++ selenium_form.py
@@ -0,0 +1,6 @@
+# Open https://the-internet.herokuapp.com/login
+
+# Please automate next test cases:
+# 1. Login with valid creds (tomsmith/SuperSecretPassword!) and assert you successfully logged in
+# 2. Login with invalid creds and check validation error
+# 3. Logout from app and assert you successfully logged out

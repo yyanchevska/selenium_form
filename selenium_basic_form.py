56164dd131edd3aeff93bffe15213c3f984a06f4
“yyanchevska” <yana.yanchevska10@gmail.com>
2020-07-27 16:40:41 +0300

    Selenium_basic_form

--- selenium_basic_form.py
+++ selenium_basic_form.py
@@ -25,3 +25,21 @@ driver.find_element(By.CSS_SELECTOR, "form#get-input button").click()
 
 assert driver.find_element(By.ID, "display").text == TEST_DATA['single_input']
 driver.quit()
+
+
+def test1():
+    driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
+
+    driver.find_element(By.CSS_SELECTOR, "input#user-message").send_keys(TEST_DATA['single_input'])
+    driver.find_element(By.CSS_SELECTOR, "form#get-input button").click()
+    assert "My best message here!!" in driver.page_source
+    driver.quit()
+
+
+def test2():
+    driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
+    driver.find_element(By.CSS_SELECTOR, "#sum1").send_keys(TEST_DATA['two_input_first'])
+    driver.find_element(By.CSS_SELECTOR, "#sum2").send_keys(TEST_DATA['two_input_second'])
+    driver.find_element(By.CSS_SELECTOR, "#gettotal > button").click()
+    assert "707" in driver.page_source
+    driver.quit()
